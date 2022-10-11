4516a800a458db05a2d090e557a740625da55792
LING_ZI_QING <onlylingstudio@gmail.com>
2022-10-11 09:41:37 +0800

    fix: 🐛 babel 配置文件

--- template/babel.config.js
+++ template/babel.config.js
@@ -1,3 +1,36 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-};
+  plugins: [
+    [
+      'import',
+      {
+        libraryName: '@fruits-chain/react-native-xiaoshu',
+        libraryDirectory: 'src',
+      },
+      '@fruits-chain/react-native-xiaoshu',
+    ],
+    [
+      'import',
+      {
+        libraryName: 'lodash',
+        libraryDirectory: '',
+        camel2DashComponentName: false,
+      },
+      'lodash',
+    ],
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@': './src',
+        },
+      },
+    ],
+  ],
+  env: {
+    production: {
+      plugins: ['transform-remove-console'],
+    },
+  },
+}
