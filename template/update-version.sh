1d6ed8a7b4a2fef8664008e334ae781dca97e937
LING_ZI_QING <onlylingstudio@gmail.com>
2023-02-24 09:54:50 +0800

    feat: 🎸 版本更新统一通过脚本操作

--- template/update-version.sh
+++ template/update-version.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash -e
 
-PROJECT_DIR="ios/ProjectName"
-INFOPLIST_FILE="Info.plist"
-INFOPLIST_DIR="${PROJECT_DIR}/${INFOPLIST_FILE}"
-
 PACKAGE_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]')
 PACKAGE_VERSION_CODE=$(cat package.json | grep versionCode | head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]')
 
+## iOS
+PROJECT_IOS_DIR="ios/ProjectName"
+INFOPLIST_FILE="Info.plist"
+INFOPLIST_DIR="${PROJECT_IOS_DIR}/${INFOPLIST_FILE}"
 
 # Update plist with new values
 /usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString ${PACKAGE_VERSION}" "${INFOPLIST_DIR}"
 /usr/libexec/PlistBuddy -c "Set :CFBundleVersion ${PACKAGE_VERSION_CODE}" "${INFOPLIST_DIR}"
+
+## Android
+BUILD_GRADLE="android/app/build.gradle"
+
+sed -i '' "s/versionName \"[0-9.]*\"/versionName \"${PACKAGE_VERSION}\"/g" "${BUILD_GRADLE}"
+sed -i '' "s/versionCode [0-9]*/versionCode ${PACKAGE_VERSION_CODE}/g" "${BUILD_GRADLE}"
